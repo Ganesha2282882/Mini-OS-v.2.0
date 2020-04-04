5ed3a8e280f46b86473cfa38773b26d0efbfa3d9
Ganesha Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2020-04-04 14:13:54 -0400

    Update Bin.py

--- Bin.py
+++ Bin.py
@@ -6,7 +6,7 @@ def options():
     if showops == "op1":
         os.system("python3 Op1.py")
 
-    elif showops == "op1":
+    elif showops == "op2":
         os.system("python3 Op2.py")
 
     else:
